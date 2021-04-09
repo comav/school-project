652480a847ca0751b4fcffb9b847921c2bed8a7e
comav <tsyomav@gmail.com>
2021-04-09 20:18:57 +0300

    New background and some mobile improvements

--- scripts/index.js
+++ scripts/index.js
@@ -2,64 +2,22 @@
 
 new Splide('.splide').mount();
 
-//anime.js animations
+//vanta.js
 
-anime({
-    targets: '.leaf1',
-    translateX: 1200,
-    translateY: [randomY(1), randomY(1), randomY(1), randomY(1)],
-    rotate: [0, randomDeg()],
-    delay: 1500,
-    direction: 'normal',
-    loop: true,
-    easing: 'linear',
-    opacity: [0.5, 1, 0],
-    duration: 5000
-})
-
-anime({
-    targets: '.leaf2',
-    translateX: 1200,
-    translateY: [randomY(2), randomY(2), randomY(2), randomY(2), randomY(2)],
-    rotate: [0, randomDeg()],
-    delay: 3000,
-    direction: 'normal',
-    loop: true,
-    easing: 'linear',
-    opacity: [0.5, 1, 0],
-    duration: 5000
-})
-
-anime({
-    targets: '.leaf3',
-    translateX: 1200,
-    translateY: [100, -55, 34],
-    rotate: [0, randomDeg()],
-    delay: 1000,
-    direction: 'normal',
-    loop: true,
-    easing: 'linear',
-    opacity: [0, 1, 0],
-    duration: 5000
-})
+VANTA.TOPOLOGY({
+    el: ".forest",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x00c97f,
+    backgroundColor: 0x00
+  })
 
 //functions
-
-function randomY(leafNum) {
-    if (leafNum == 1) {
-        return Math.floor(Math.random() * (100 - 50 + 1)) + 100;
-    } if (leafNum == 2) {
-        return Math.floor(Math.random() * (200 - 100 + 1)) + 100;
-    } if (leafNum == 3) {
-        return Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-    } else {
-        console.error(`There's no leaf $(leafNum)`);
-    }
-}
-
-function randomDeg() {
-    return Math.floor(Math.random() * (360 - (-360) + 1)) + (-360);
-}
 
 function notReady () {
     alert('Цей функціонал недоступний');
